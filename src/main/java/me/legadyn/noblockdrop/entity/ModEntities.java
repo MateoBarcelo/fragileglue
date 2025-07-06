fc41dec8c4cee4c67dc206deca4a24497eb1bdd4
MateoBarcelo <mateobarcelo88@gmail.com>
2025-07-05 22:53:06 -0300

    refactor glue entity hitbox size

--- src/main/java/me/legadyn/noblockdrop/entity/ModEntities.java
+++ src/main/java/me/legadyn/noblockdrop/entity/ModEntities.java
@@ -14,7 +14,7 @@ public class ModEntities {
     public static final RegistryObject<EntityType<NoBlockDropGlueEntity>> CUSTOM_GLUE =
             ENTITIES.register("fragile_glue", () ->
                     EntityType.Builder.<NoBlockDropGlueEntity>of(NoBlockDropGlueEntity::new, MobCategory.MISC)
-                            .sized(1.05f, 1.05f)            .clientTrackingRange(10)
+                            .sized(0.5f, 0.5f)            .clientTrackingRange(10)
                             .updateInterval(20)
                             .build("fragile_glue")
             );
